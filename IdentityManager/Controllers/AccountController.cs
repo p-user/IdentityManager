9ea1ec2f18131674ae6d689d042bb62a54d2b9dd
pammy <pamelaagaj@gmail.com>
2022-08-25 15:59:25 +0100

    Initial Commit

---
+++ IdentityManager/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signinManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signinManager)
+        {
+            _userManager=userManager;
+            _signinManager=signinManager;   
+        }
+
+        public IActionResult Index() 
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerviewModel = new RegisterViewModel();
+            return View(registerviewModel); 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName=model.Email, Email = model.Email , Name=model.Name};
+                var result = await _userManager.CreateAsync(user, model.Password);  
+                if(result.Succeeded)
+                {
+                    await _signinManager.SignInAsync(user, isPersistent: false); 
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+                
+            } 
+            return View(model);
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        public async Task<IActionResult> LogOff()
+        {
+            await _signinManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index),"Home");
+        }
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid) {
+                var result=await _signinManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,lockoutOnFailure:false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid Login");
+                    return View(model);
+                }
+            }  
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
